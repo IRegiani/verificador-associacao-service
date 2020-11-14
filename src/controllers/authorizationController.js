df0f3ee86f45dd39a09e16533356c57b928e57d6
Iago Regiani <iagoregiani@gmail.com>
2020-11-14 18:49:56 -0300

    added db test

--- src/controllers/authorizationController.js
+++ src/controllers/authorizationController.js
@@ -6,6 +6,7 @@ module.exports = () => {
   const AuthorizationController = {
     async login(request, response, next) {
       const { body: { username, password } } = request;
+      const { db } = response.locals;
 
       try {
         logger.info('Trying to login', { username });
@@ -14,8 +15,17 @@ module.exports = () => {
         if (!password) throw new CustomError('Missing password', StatusCodes.BAD_REQUEST);
 
         // TODO: call authorization service to handle authentication logic
+        // DB example
+        const doc = await db.doc('logins/history');
+        const oldLogins = await doc.get();
+        const logins = (oldLogins.data() && oldLogins.data().entries) || [];
 
-        return response.json({ message: 'Success' });
+        const newLogin = { username, timestamp: new Date().toISOString() };
+
+        const newEntry = [...logins, newLogin];
+        await doc.update({ entries: newEntry });
+
+        return response.json({ message: 'Success', logins: newEntry });
       } catch (error) {
         if (isExpectedError(error)) return handleError(response, error, logger);
 
